629c0a9b4aff7ab869587f55c3eb02da02f4b8e2
Vishwas <83687368+Vishwas1810@users.noreply.github.com>
2022-11-19 00:20:29 +0530

    Create config.php

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+	$databaseHost = 'localhost';
+	$databaseName = 'workshop1';
+	$databaseUsername = 'root';
+	$databasePassword = '';
+
+	$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
+ 
+?>
